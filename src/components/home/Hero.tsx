f0d0e8d5411bdf77246a34a138701373f04acab1
narminasadoffa-prog <narminasadoffa@gmail.com>
2025-10-30 18:09:20 +0400

    Hero: make background YouTube video fully cover viewport using responsive 16:9 max() sizing (no side gaps)

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -19,10 +19,14 @@ const Hero = () => {
       >
         <div className="w-full h-full relative">
           <iframe
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full w-auto h-auto"
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
             src="https://www.youtube.com/embed/5qbjKpxfD64?autoplay=1&mute=1&controls=0&loop=1&modestbranding=1&playsinline=1&showinfo=0&rel=0&iv_load_policy=3&playlist=5qbjKpxfD64&fs=0&disablekb=1"
             title="Hero Background Video"
             allow="autoplay; encrypted-media; picture-in-picture"
+            style={{
+              width: 'max(100vw, 177.78vh)', // 16:9 => 16/9*100vh
+              height: 'max(56.25vw, 100vh)', // 9/16*100vw
+            }}
           />
         </div>
       </div>
