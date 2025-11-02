e72c66825e3e383c1f47fde4049112eceaae3914
narminasadoffa-prog <narminasadoffa@gmail.com>
2025-11-02 16:22:44 +0400

    Qar effektini silme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,10 @@ import FeaturedCars from '@/components/home/FeaturedCars';
 import Reviews from '@/components/home/Reviews';
 import FeedbackForm from '@/components/home/FeedbackForm';
 import CTASection from '@/components/home/CTASection';
-import SnowEffect from '@/components/home/SnowEffect';
 
 const Home = () => {
   return (
     <>
-      <SnowEffect />
       <Hero />
       <FeaturedCars />
       <Services />
